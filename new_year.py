169612f8303129c013eec6cbb6afda722dc2bd75
Antoine Augusti <antoine.augusti@gmail.com>
2018-10-08 10:08:24 +0200

    Add script to generate an empty new year

---
+++ new_year.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from argparse import ArgumentParser
+from datetime import date
+from datetime import timedelta
+
+parser = ArgumentParser()
+parser.add_argument('target_year', help='Year we need to generate', type=int)
+args = parser.parse_args()
+
+target_year = args.target_year
+
+current_date = date(target_year, 1, 1)
+
+while current_date.year == target_year:
+    current_date = current_date + timedelta(days=1)
+    date = current_date.strftime('%Y-%m-%d')
+    print(date + ',False,False,False,')
